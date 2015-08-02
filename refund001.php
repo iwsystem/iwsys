bfebf6bbbe2090993d7ee09d271468b950991f1d
IW System <tech@iwsystem.co.uk>
2015-08-02 23:22:52 +0100

    Updating the refund policywith chargebacks

--- refund001.php
+++ refund001.php
@@ -40,11 +40,8 @@
     <section id="privacy" class="container">
         <div class="row-fluid">
             <div class="span7">
-            <h4>CANCELLATIONS</h4><BR>
                 <p>We value the satisfaction of our clients and are committed to serving the best interest of our clients as well.</p>
-                <p>Due to the nature of the services that are being offered in progressive phases which include but not limited to consulting, 
-                analysis, design, development, coding, configuration, implementation, maintenance and administration of website, web application or software solutions, 
-                at IW System we apply a strict refund policy for our services as they are offered as customized and as specified according to Client's requirement.</p>
+                <p>Due to the nature of the services being offered, which includes consulting, analysis, design, development, coding, configuration, implementation, maintenance and administration of website, web application or software solutions, we at IW System apply a strict refund policy for the services offered.</p>
                 <p>Our terms for refunds are mentioned herein:</p>
                 <ul class=''>
                     <li><b>If Client cancels the project 7 days immediately after making initial payment, before the first phase of the project is commenced, they are entitled to a full refund</b></li><br>
@@ -54,20 +51,6 @@
                     <li><b>The Project Total Cost is defined as the total amount of the cost of the project based upon Client expectations and requests.</b></li><br>
                     <li><b>Should Client fail to respond to various communication requests for information for a period of 30 days, while the project has been initiated, Client’s account will be considered inactive and the Project will terminate. No refunds or credits will be given for any payments made for work performed.</b></li><br>
                 </ul>
-                <br>
-                <h4>CHARGEBACKS</h4><br>
-
-                <p>Chargebacks are not considered as a way of refunding. Any Client who initiates a credit card chargeback in response to an order placed with IW System will be subject to full investigation.</p>
-
-                <p>When choosing to contract us for your project and making payment online within our system, Clients agree to the conditions explicated in our Service Level Agreement & the Terms of Service. Thus all investigations will resort to these documents as an evidence for the Client's agreement, especially in the event that a Client sends a chargeback inquiry based on points that he /or she first accepted, but is trying now to refute.</p>
-
-                <p>If/When a chargeback report is received, the first step that our support team at IW System will do is try to contact the user. Meanwhile, the user account will be suspended (in order to protect our system from fraudulent activity and misuse) and will remain suspended until the reasons for the chargeback have been clarified and decision has been taken.</p>
-
-                <p>If the chargeback has occurred due to some misunderstanding, a reverse payment procedure will be initiated with our authorized payment processor and the account will be re-activated.</p>
-
-                <p>If the user has sent a chargeback request based on groundless argumentation (to our own judgment and based on the Terms of Service), then a dispute will be started with our authorized processors in which the Terms of Service and the Service Level Agreement of IW System will serve as proof of the Client's agreement. The processors then, taking into consideration all provisions in these legal documents, will decide whether the Client has had sufficient grounds for the chargeback or IW System has the right to a reverse payment.</p>
-
-                <p>In case that the chargeback originates from a fraudulent user and no reverse payment procedure can be initiated, then the respective hosting account will remain suspended.</p>
             </div>
             <div class="span2"></div>
             <div class="span3">
